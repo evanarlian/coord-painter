deac24ac61599a47aa9c7bf88b73287e6a52c9cd
evanarlian <53576267+evanarlian@users.noreply.github.com>
2021-03-25 14:13:41 +0700

    check io function

---
+++ convert.py
@@ -0,0 +1,57 @@
+import sys
+from os import path, read
+from argparse import ArgumentParser
+
+
+import PIL
+from PIL import Image
+import numpy as np
+
+
+parser = ArgumentParser()
+parser.add_argument("image", type=str, help="any image file")
+parser.add_argument("-o", "--output", type=str, help="output name in npz format")
+args = parser.parse_args()
+
+
+def determine_path(args):
+
+	dir_name = path.dirname(args.image)
+	file_name, file_ext = path.splitext(path.basename(args.image))
+
+	if args.output is not None:
+		dir_name = dir_name = path.dirname(args.output)
+		file_name, file_ext = path.splitext(path.basename(args.output))
+
+	read_from = args.image
+	save_to = path.join(dir_name, file_name + file_ext)
+	
+	return read_from, save_to
+
+
+def read_image(image_path):
+
+	try:
+		image = Image.open(image_path)
+	except PIL.UnidentifiedImageError:
+		print("PIL cannot read the image.")
+		sys.exit(1)
+	except FileNotFoundError:
+		print("File not found.")
+		sys.exit(1)
+	except:
+		print("Unknown error.")
+		sys.exit(1)
+
+	image = np.array(image)
+	return image
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	read_from, save_to = determine_path(args)
+	
